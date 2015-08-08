09553825b6298867e3fdab112011ab152fb1c6e9
capt-MAKO <mayankdcoder@gmail.com>
2015-08-08 19:20:28 +0530

    Create wall.php

---
+++ Pages/wall.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+		<TITLE>ISTE, KNIT Sultanpur</TITLE>
+	</HEAD>
+	<BODY>
+		<A HREF="../index.php">&larr; Back</A><BR /><BR />
+		You nedd to be logged in to view the contents of this page!!
+	</BODY>
+</HTML>
