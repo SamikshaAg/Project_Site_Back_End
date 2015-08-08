01c63d5688907c927dfe838754cbf1fa310d0ca0
capt-MAKO <mayankdcoder@gmail.com>
2015-08-08 19:20:56 +0530

    Create public.php

---
+++ Pages/public.php
@@ -0,0 +1,13 @@
+<?php
+	include '../Core/init.php' ;
+?>
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+		<TITLE>ISTE, KNIT Sultanpur</TITLE>
+	</HEAD>
+	<BODY>
+		<A HREF="../index.php">&larr; Back</A><BR /><BR />
+		This is a Public page!!
+	</BODY>
+</HTML>
