37b91a36009aaa8bb408f40941b5554aa0fdfbdc
capt-MAKO <mayankdcoder@gmail.com>
2015-08-10 01:04:55 +0530

    Update connection.php

--- Core/connection.php
+++ Core/connection.php
@@ -1,12 +1,9 @@
 <?php
-	//user details
+	//server details
 	$host = '127.0.0.1' ;
 	$user = 'root' ;
 	$password = '' ;
 	$database= 'site_test' ;
-	//generic connection error message
-	$connection_error = 'Sorry can\'t connect to database' ;
-	//connection to the database
-	mysql_connect($host , $user , $password) or die($connection_error) ;
-	mysql_select_db($database) ;
+	//databse connection through mysqli
+	$database_handler = new mysqli($host , $user , $password , $database)or die("Could not connect to mysql".mysqli_error($database_handler)) ;
 ?>
