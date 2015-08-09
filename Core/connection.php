54c23fb030912e02a33b839068011ef52425ba08
capt-MAKO <mayankdcoder@gmail.com>
2015-08-09 20:16:54 +0530

    Update connection.php

--- Core/connection.php
+++ Core/connection.php
@@ -7,5 +7,6 @@
 	//generic connection error message
 	$connection_error = 'Sorry can\'t connect to database' ;
 	//connection to the database
-	$database_handler = new mysqli($host , $user , $password , $database) or die($connection_error) ;
+	mysql_connect($host , $user , $password) or die($connection_error) ;
+	mysql_select_db($database) ;
 ?>
